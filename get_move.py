082da2152e31d435c79b26d0dbe875b867d372b4
ibrahemkhafaga <77680175+ibrahemkhafaga@users.noreply.github.com>
2021-06-05 04:25:53 +0200

    Add files via upload

---
+++ get_move.py
@@ -0,0 +1,15 @@
+ def get_move(state):
+    #TODO
+    return move  
+
+
+
+'''
+node attrbutes :
+1) leaf --> equals one if the node is leaf , otherwise zero
+2) cost --> equals the estimated cost 
+3) mini_max --> equals 'max' if the node is maximizer and equals 'min' if the node is minimizer
+4) children --> list contains childs of the node
+5) state --> list contains state this node represnt 
+6) pruned --> equals one if the node is pruned and zero other wise
+'''
